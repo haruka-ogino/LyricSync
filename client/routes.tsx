8ed7d48fb473de2c0060ad68e48a0a942adcb6d2
Haruka Ogino <harukariq@gmail.com>
2024-05-03 14:10:01 +1200

    create Home page component

--- client/routes.tsx
+++ client/routes.tsx
@@ -1,3 +1,8 @@
 import { createRoutesFromElements, Route } from 'react-router-dom'
-import App from './components/App.tsx'
-export default createRoutesFromElements(<Route index element={<App />} />)
+import Layout from './components/Layout.tsx'
+import Home from './pages/Home.tsx'
+export default createRoutesFromElements(
+  <Route path="/" element={<Layout />}>
+    <Route index element={<Home />} />
+  </Route>,
+)
