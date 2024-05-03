18f01e9b33cb8716946e781cfe1cab40bc41bdad
Haruka Ogino <harukariq@gmail.com>
2024-05-03 14:13:47 +1200

    create Collections comp

---
+++ client/pages/Collections.tsx
@@ -0,0 +1,7 @@
+export default function Collections() {
+  return (
+    <>
+      <h1>Collections</h1>
+    </>
+  )
+}
--- client/routes.tsx
+++ client/routes.tsx
@@ -1,8 +1,10 @@
 import { createRoutesFromElements, Route } from 'react-router-dom'
 import Layout from './components/Layout.tsx'
 import Home from './pages/Home.tsx'
+import Collections from './pages/Collections.tsx'
 export default createRoutesFromElements(
   <Route path="/" element={<Layout />}>
     <Route index element={<Home />} />
+    <Route path="my-collections" element={<Collections />} />
   </Route>,
 )
