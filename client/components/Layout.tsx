e5eee1720f3060133a6826b452a4ec2eaeb84152
Haruka Ogino <harukariq@gmail.com>
2024-05-03 14:49:25 +1200

    create nav to check routes

--- client/components/Layout.tsx
+++ client/components/Layout.tsx
@@ -1,4 +1,5 @@
 import { Outlet } from 'react-router-dom'
+import Nav from './Nav'
 
 export default function Layout() {
   return (
@@ -7,6 +8,7 @@ export default function Layout() {
         <h1>LyricSync</h1>
       </header>
       <main>
+        <Nav />
         <Outlet />
       </main>
       <footer></footer>
