b27e7b927edd00ea614775561bb4caedce6481a1
Haruka Ogino <harukariq@gmail.com>
2024-05-04 17:02:25 +1200

    update lyrics model

---
+++ models/songs.ts
@@ -0,0 +1,20 @@
+export interface Fruit {
+  id: number
+  name: string
+}
+
+export interface FruitData {
+  name: string
+}
+
+export interface Lyrics {
+  songId: number
+  songTitle: string
+  originLang: string
+  transLang: string
+  originLyrics: string
+  translatedLyrics: string
+  romanisation: string
+  romanisedLyrics: string
+  collectionId: number
+}
