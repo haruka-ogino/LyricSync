ff4b36103e57f61127533f1fa18112baca5e143e
Haruka Ogino <harukariq@gmail.com>
2024-05-04 17:01:23 +1200

    update songs fn backend

--- server/routes/songs.ts
+++ server/routes/songs.ts
@@ -1,12 +1,14 @@
 import { Router } from 'express'
-import * as db from '../db/collections'
+import * as db from '../db/songs'
 const router = Router()
 
 // getLyrics by songId
 router.get('/:collectionId/:songId', async (req, res) => {
   try {
+    const collectionId = Number(req.params.collectionId)
     const songId = Number(req.params.songId)
     const lyrics = await db.getLyrics(songId)
+    // if (lyrics.collec)
     res.json(lyrics)
   } catch (error) {
     console.log(error)
