4a52a1ac900554c0385dd67d0c95e90097ed0f10
Moa Ha <moahaa03@gmail.com>
2024-05-04 15:38:02 +1200

    fix typos

--- server/db/seeds/5-lyrics.js
+++ server/db/seeds/5-lyrics.js
@@ -6,7 +6,7 @@ export async function seed(knex) {
   await knex('lyrics').insert([
     {
       id: 1,
-      original_lan: 1,
+      original_lang: 1,
       trans_lang: 2,
       original_lyric: 'Every night in my dreams\n\n I see you, I feel you',
       trans_lyric: 'test - 테스트',
