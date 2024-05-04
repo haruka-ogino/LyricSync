cada6f60dcdf3229b7c08e3b6d006ed65489d456
Haruka Ogino <harukariq@gmail.com>
2024-05-04 15:57:50 +1200

    able to retrieve data so far

--- server/db/collections.ts
+++ server/db/collections.ts
@@ -10,18 +10,20 @@ export async function getCollections(): Promise<Collection[]> {
 export async function getLyrics(songId: number): Promise<Collection[]> {
   return db('lyrics')
     .join('songs', 'songs.id', 'song_id')
-    .join('collections', 'collections.id', 'playlist_id')
-    .join('languages as originLang', 'languages.id', 'original_lang')
-    .join('languages as transLang', 'languages.id', 'trans_lang')
     .where('song_id', songId)
-    .select(
-      'lyrics.song_id as songId',
-      'songs.title as songTitle',
-      'originLang.name as originLang',
-      'transLang.name as transLang',
-      'lyrics.original_lyric as originLyrics',
-      'lyrics.trans_lyric as translatedLyrics',
-      'lyrics.romanisation as romanisation',
-      'lyrics.romanised_text as romanisedLyrics',
-    )
+
+  // .join('collections', 'collections.id', 'playlist_id')
+  // .join('languages as originLang', 'languages.id', 'original_lang')
+  // .join('languages as transLang', 'languages.id', 'trans_lang')
+  // .where('song_id', songId)
+  // .select(
+  //   'lyrics.song_id as songId',
+  //   'songs.title as songTitle',
+  //   'originLang.name as originLang',
+  //   'transLang.name as transLang',
+  //   'lyrics.original_lyric as originLyrics',
+  //   'lyrics.trans_lyric as translatedLyrics',
+  //   'lyrics.romanisation as romanisation',
+  //   'lyrics.romanised_text as romanisedLyrics',
+  // )
 }
