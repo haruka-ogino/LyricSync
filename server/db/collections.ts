036e9bc27e59fe1d083da36336a0ddf61b3a308c
Haruka Ogino <harukariq@gmail.com>
2024-05-04 15:47:06 +1200

    get Lyrics fn complete

--- server/db/collections.ts
+++ server/db/collections.ts
@@ -7,6 +7,21 @@ export async function getCollections(): Promise<Collection[]> {
   return db('collections')
 }
 
-export async function getLyrics(songId): Promise<Collection[]> {
-  return db('collections')
+export async function getLyrics(songId: number): Promise<Collection[]> {
+  return db('lyrics')
+    .join('songs', 'songs.id', 'song_id')
+    .join('collections', 'collections.id', 'playlist_id')
+    .join('languages as originLang', 'languages.id', 'original_lang')
+    .join('languages as transLang', 'languages.id', 'trans_lang')
+    .where('song_id', songId)
+    .select(
+      'lyrics.song_id as songId',
+      'songs.title as songTitle',
+      'originLang.name as originLang',
+      'transLang.name as transLang',
+      'lyrics.original_lyric as originLyrics',
+      'lyrics.trans_lyric as translatedLyrics',
+      'lyrics.romanisation as romanisation',
+      'lyrics.romanised_text as romanisedLyrics',
+    )
 }
--- server/routes/collections.ts
+++ server/routes/collections.ts
@@ -17,6 +17,7 @@ router.get('/:collectionId/:songId', async (req, res) => {
   try {
     const songId = Number(req.params.songId)
     const lyrics = await db.getLyrics(songId)
+    res.json(lyrics)
   } catch (error) {
     console.log(error)
     res.status(500).json({ message: 'Something went wrong' })
