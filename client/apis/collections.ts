1973063c0efd2226034d97f7f0ef2f838ea8a836
Moa Ha <moahaa03@gmail.com>
2024-05-04 16:32:53 +1200

    make frontend route for add function

--- client/apis/collections.ts
+++ client/apis/collections.ts
@@ -1,9 +1,13 @@
 import request from 'superagent'
-import { Collection } from '../../models/collections'
+import { Collection, CollectionData } from '../../models/collections'
 
 const rootUrl = '/api/v1/collections'
 
 export async function getCollections() {
   const res = await request.get(rootUrl)
   return res.body as Collection[]
+}
+
+export async function addCollection(data: CollectionData) {
+  await request.post(rootUrl).send(data)
 }
--- client/hooks/useCollections.ts
+++ client/hooks/useCollections.ts
@@ -1,9 +1,18 @@
-import { useQuery } from '@tanstack/react-query'
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query'
 import * as api from '../apis/collections'
+import { CollectionData } from '../../models/collections'
 
 export function useCollections() {
   return useQuery({
     queryKey: ['collections'],
     queryFn: api.getCollections,
+  })
+}
+
+export function useAddCollection() {
+  const qc = useQueryClient()
+  return useMutation({
+    mutationFn: (data: CollectionData) => api.addCollection(data),
+    onSuccess: () => qc.invalidateQueries({ queryKey: ['newCollection'] }),
   })
 }
