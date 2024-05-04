3a499e622dcd6e7f0e4d7d37153d694d05dec9e7
Moa Ha <moahaa03@gmail.com>
2024-05-04 13:53:59 +1200

    make tables

---
+++ server/db/migrations/4-languages.js
@@ -0,0 +1,18 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export async function up(knex) {
+  return knex.schema.createTable('languages', (table) => {
+    table.increments('id').primary
+    table.string('name')
+  })
+}
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export async function down(knex) {
+  return knex.schema.dropTable('languages')
+}
