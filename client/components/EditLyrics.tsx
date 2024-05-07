2cd412779d4db88daaac6de64694e5bc6174f30a
Haruka Ogino <harukariq@gmail.com>
2024-05-07 15:44:40 +1200

    create edit lyrics component

---
+++ client/components/EditLyrics.tsx
@@ -0,0 +1,16 @@
+import { useEditLyrics } from '../hooks/useSongs'
+
+interface Id {
+  id: number
+}
+
+function EditLyrics({ id }: Id) {
+  const mutation = useEditLyrics()
+
+  function handleClick() {
+    mutation.mutate(id)
+  }
+  return <button onClick={handleClick}>Edit Lyrics</button>
+}
+
+export default EditLyrics
