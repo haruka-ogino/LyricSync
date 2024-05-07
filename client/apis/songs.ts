0e6ada06c8d66b08a93f8fed2e284a4683a650be
Haruka Ogino <harukariq@gmail.com>
2024-05-07 15:33:04 +1200

    create front end call to edit lyrics

--- client/apis/songs.ts
+++ client/apis/songs.ts
@@ -1,16 +1,14 @@
 import request from 'superagent'
-import { Lyrics } from '../../models/songs'
+import { EditedLyrics, Lyrics } from '../../models/songs'
 
-const rootUrl = '/api/v1'
+const rootUrl = '/api/v1/collections'
 
 export async function getLyrics(
   songId: number,
   collectionId: number,
 ): Promise<Lyrics | undefined> {
   try {
-    const res = await request.get(
-      rootUrl + `/collections/${collectionId}/${songId}`,
-    )
+    const res = await request.get(rootUrl + `/${collectionId}/${songId}`)
 
     if (!res.ok) {
       throw new Error(`Failed to fetch lyrics (${res.status}): ${res.error}`)
@@ -18,29 +16,19 @@ export async function getLyrics(
 
     return res.body as Lyrics
   } catch (error) {
-    console.error('Error fetching lyrics:', error)
+    console.error('Error fetching lyrics.')
     throw new Error('Failed to fetch lyrics. Please try again.')
   }
 }
 
-// export async function editLyrics(
-//   songId: number,
-//   collectionId: number,
-//   data:Lyrics
-// ): Promise<Lyrics | undefined> {
-//   try {
-//     const { id } = data
-//     const res = await request
-//       .patch(rootUrl + `/characters/update/${id}`)
-//       .send(data)
+export async function editLyrics(data: EditedLyrics) {
+  try {
+    const { id } = data
+    const res = await request.patch(rootUrl + `/update/${id}`).send(data)
 
-//     if (!res.ok) {
-//       throw new Error(`Failed to fetch lyrics (${res.status}): ${res.error}`)
-//     }
-
-//     return res.body as Lyrics
-//   } catch (error) {
-//     console.error('Error fetching lyrics:', error)
-//     throw new Error('Failed to fetch lyrics. Please try again.')
-//   }
-// }
+    return res.body as Lyrics
+  } catch (error) {
+    console.error('Error editing lyrics.')
+    throw new Error('Failed to edit lyrics. Please try again.')
+  }
+}
