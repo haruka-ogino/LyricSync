a6cf1a22a87a53b596c8fd6926aece12165ef6b7
Haruka Ogino <harukariq@gmail.com>
2024-05-07 16:13:47 +1200

    remove comments

--- server/routes/songs.ts
+++ server/routes/songs.ts
@@ -22,44 +22,6 @@ router.get('/:collectionId/:songId', async (req, res) => {
   }
 })
 
-// router.patch('/update/origin/:id', async (req, res) => {
-//   try {
-//     const data = req.body
-//     const { currentId, originLyrics, originLang } = data
-//     const newLyrics = {
-//       id: currentId,
-//       original_lyric: originLyrics,
-//       original_lang: originLang,
-//     }
-//     const id = Number(req.params.id)
-//     const edited = await db.editLyrics(id, newLyrics)
-
-//     res.status(200).json(edited)
-//   } catch (error) {
-//     console.log(error)
-//     res.status(500).json({ message: 'Something went wrong' })
-//   }
-// })
-
-// router.patch('/update/translated/:id', async (req, res) => {
-//   try {
-//     const data = req.body
-//     const { currentId, translatedLyrics, transLang } = data
-//     const newLyrics = {
-//       id: currentId,
-//       trans_lyric: translatedLyrics,
-//       trans_lang: transLang,
-//     }
-//     const id = Number(req.params.id)
-//     const edited = await db.editLyrics(id, newLyrics)
-
-//     res.status(200).json(edited)
-//   } catch (error) {
-//     console.log(error)
-//     res.status(500).json({ message: 'Something went wrong' })
-//   }
-// })
-
 router.patch('/update/:lang/:id', async (req, res) => {
   try {
     const data = req.body
