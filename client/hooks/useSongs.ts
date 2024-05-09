f2fe19c8760be25055461ee78f4ce93be1983478
Moa Ha <moahaa03@gmail.com>
2024-05-09 13:50:52 +1200

    add frontnd route

--- client/hooks/useSongs.ts
+++ client/hooks/useSongs.ts
@@ -1,6 +1,6 @@
 import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query'
-import { editLyrics, getLyrics } from '../apis/songs.ts'
-import { EditedLyrics } from '../../models/songs.ts'
+import { addSong, editLyrics, getLyrics } from '../apis/songs.ts'
+import { EditedLyrics, SongData } from '../../models/songs.ts'
 
 export function useLyrics(songId: number, collectionId: number) {
   return useQuery({
@@ -14,5 +14,13 @@ export function useEditLyrics() {
   return useMutation({
     mutationFn: (lyrics: EditedLyrics) => editLyrics(lyrics),
     onSuccess: () => qc.invalidateQueries({ queryKey: ['lyrics'] }),
+  })
+}
+
+export function useAddSong() {
+  const qc = useQueryClient()
+  return useMutation({
+    mutationFn: (input: SongData) => addSong(input),
+    onSuccess: () => qc.invalidateQueries({ queryKey: ['newSong'] }),
   })
 }
