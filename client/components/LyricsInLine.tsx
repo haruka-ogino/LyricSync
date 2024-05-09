e957c72819e52c863921c36ae1527b9fcc17a175
Haruka Ogino <harukariq@gmail.com>
2024-05-09 13:45:32 +1200

    create new arrays for translated and original lyrics

--- client/components/LyricsInLine.tsx
+++ client/components/LyricsInLine.tsx
@@ -6,13 +6,24 @@ interface Props {
 export default function LyricsInLine({ lyrics }: Props) {
   // to display line by line,
   // 1. find \n\n and replace it with '\nspace\n'
-  console.log(lyrics.translatedLyrics)
+  // console.log(lyrics.translatedLyrics)
   // console.log(lyrics.originLyrics)
 
   const newTraLyrics = lyrics.translatedLyrics.replace(/\n\n/g, '\nspace\n')
-  console.log(newTraLyrics)
+  // console.log(newTraLyrics)
+
+  const newOriLyrics = lyrics.originLyrics.replace(/\n\n/g, '\nspace\n')
+  // console.log(newOriLyrics)
+
   // 2. make an array for each set or lyrics (or and tr)
   // where the end of each line is denoted by \n
+
+  const translatedArr = newTraLyrics.split('\n')
+  // console.log(translatedArr)
+
+  const originalArr = newOriLyrics.split('\n')
+  // console.log(originalArr)
+
   // 3. display the lyrics by mapping through the or array
   // using the index from the map method to to also display the tr array
   // 4. style these two lines differently
