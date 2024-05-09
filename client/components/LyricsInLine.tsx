97f17f1068cb8862d61229e94ed28f2d35209b22
Haruka Ogino <harukariq@gmail.com>
2024-05-09 13:42:06 +1200

    replace \n\n on strings

--- client/components/LyricsInLine.tsx
+++ client/components/LyricsInLine.tsx
@@ -4,6 +4,21 @@ interface Props {
   lyrics: Lyrics
 }
 export default function LyricsInLine({ lyrics }: Props) {
+  // to display line by line,
+  // 1. find \n\n and replace it with '\nspace\n'
+  console.log(lyrics.translatedLyrics)
+  // console.log(lyrics.originLyrics)
+
+  const newTraLyrics = lyrics.translatedLyrics.replace(/\n\n/g, '\nspace\n')
+  console.log(newTraLyrics)
+  // 2. make an array for each set or lyrics (or and tr)
+  // where the end of each line is denoted by \n
+  // 3. display the lyrics by mapping through the or array
+  // using the index from the map method to to also display the tr array
+  // 4. style these two lines differently
+  // 5. add state where user can choose how to display the data
+  // 6. implement these states
+
   return (
     <>
       <h1>In-line lyrics</h1>
