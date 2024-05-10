9ebf77adc432b3c432afb19b74268744b2504c63
Haruka Ogino <harukariq@gmail.com>
2024-05-10 16:23:36 +1200

    attempt to conditionally render navigation bar - responsive

--- client/components/Nav.tsx
+++ client/components/Nav.tsx
@@ -2,23 +2,31 @@ import { Link } from 'react-router-dom'
 import '../styles/nav.css'
 
 export default function Nav() {
+  const isMobile = window.innerWidth <= 768
+
   return (
-    <nav>
-      <Link to="collections">
-        <h2>My Collections</h2>
-      </Link>
-      <Link to="collections/X">
-        <h2>My Songs from Collection X</h2>
-      </Link>
-      <Link to="collections/1/1">
-        <h2>My Lyrics from Song Y</h2>
-      </Link>
-      <Link to="collections/new-collection">
-        <h2>Create a New Collection</h2>
-      </Link>
-      <Link to="collections/X/add-song">
-        <h2>Add Song to Collection X</h2>
-      </Link>
-    </nav>
+    <>
+      {/* {isMobile ? ( */}
+      <nav className={isMobile ? 'navbar-mobile' : 'navbar'}>
+        <Link to="collections">
+          <h2>My Collections</h2>
+        </Link>
+        <Link to="collections/X">
+          <h2>My Songs from Collection X</h2>
+        </Link>
+        <Link to="collections/1/1">
+          <h2>My Lyrics from Song Y</h2>
+        </Link>
+        <Link to="collections/new-collection">
+          <h2>Create a New Collection</h2>
+        </Link>
+        <Link to="collections/X/add-song">
+          <h2>Add Song to Collection X</h2>
+        </Link>
+      </nav>
+      {/* ) : (
+        <p></p>
+      )} */}
+    </>
   )
 }
