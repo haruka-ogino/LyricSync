376abde47b129da95445464fde939c934490a715
Moa Ha <moahaa03@gmail.com>
2024-05-12 15:14:57 +1200

    make toggle for collection list

--- client/components/Nav.tsx
+++ client/components/Nav.tsx
@@ -1,12 +1,33 @@
 import { Link } from 'react-router-dom'
 import '../styles/nav.css'
+import { useState } from 'react'
+import { useCollections } from '../hooks/useCollections'
 
 export default function Nav() {
+  const [isShow, setIsShow] = useState(false)
+  const { data } = useCollections()
+
+  function handleClick() {
+    setIsShow(!isShow)
+  }
+
   return (
     <nav>
       <Link to="collections">
         <h2>My Collections</h2>
       </Link>
+      <button onClick={handleClick}>{isShow ? '▲' : '▼'}</button>
+      {isShow && (
+        <ul>
+          {data?.map((collection) => (
+            <li key={collection.id}>
+              <Link to={`/collections/${collection.id}`}>
+                {collection.name}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      )}
       <Link to="collections/new-collection">
         <h2>Create a New Collection</h2>
       </Link>
