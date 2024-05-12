b736b9b53376b964461c2ea4724ae06f080fa973
Moa Ha <moahaa03@gmail.com>
2024-05-12 16:36:26 +1200

    style theme

--- client/components/Nav.tsx
+++ client/components/Nav.tsx
@@ -2,7 +2,7 @@ import { Link } from 'react-router-dom'
 import '../styles/nav.css'
 import { useState } from 'react'
 import { useCollections } from '../hooks/useCollections'
-import logo from '../styles/images/LyricSync-logo-C.png'
+import logo from '../styles/images/LyricSync-logo-B.png'
 
 export default function Nav() {
   const [isShow, setIsShow] = useState(false)
@@ -15,24 +15,24 @@ export default function Nav() {
   return (
     <nav>
       <img src={logo} alt="logo" className="logo" />
-      <Link to="collections">
-        <h2>My Collections</h2>
-      </Link>
-      <button onClick={handleClick}>{isShow ? '▲' : '▼'}</button>
-      {isShow && (
-        <ul>
-          {data?.map((collection) => (
-            <li key={collection.id}>
-              <Link to={`/collections/${collection.id}`}>
-                {collection.name}
-              </Link>
-            </li>
-          ))}
-        </ul>
-      )}
-      <Link to="collections/new-collection">
-        <h2>Create a New Collection</h2>
-      </Link>
+      <div>
+        <Link to="collections">My Collections </Link>
+        <button onClick={handleClick}>{isShow ? '▲' : '▼'}</button>
+        {isShow && (
+          <ul>
+            {data?.map((collection) => (
+              <li key={collection.id} className="list">
+                <Link to={`/collections/${collection.id}`}>
+                  {collection.name}
+                </Link>
+              </li>
+            ))}
+          </ul>
+        )}
+      </div>
+      <div>
+        <Link to="collections/new-collection">Create a New Collection</Link>
+      </div>
     </nav>
   )
 }
