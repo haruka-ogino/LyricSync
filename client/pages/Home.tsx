31b2be6ea3237586c5dd6c26035d409bfdcb3fc2
Moa Ha <moahaa03@gmail.com>
2024-05-15 00:46:58 +1200

    make boxes

--- client/pages/Home.tsx
+++ client/pages/Home.tsx
@@ -1,7 +1,33 @@
+import { Link } from 'react-router-dom'
+import '../styles/home.css'
+import { useCollections } from '../hooks/useCollections'
+import Delete from '../components/Delete'
+
 export default function Home() {
-  return (
-    <>
-      <h1>Home page!</h1>
-    </>
-  )
+  const { data, isLoading, isError } = useCollections()
+
+  if (isLoading) {
+    return <p>is loading...</p>
+  }
+  if (isError) {
+    return <p>Something went wrong</p>
+  }
+
+  if (data) {
+    return (
+      <>
+        <h1>Collections</h1>
+        <ul>
+          {data.map((collection, i) => (
+            <li key={i}>
+              <Link to={`/collections/${collection.id}`}>
+                {collection.name}
+              </Link>
+              <Delete id={collection.id} />
+            </li>
+          ))}
+        </ul>
+      </>
+    )
+  }
 }
