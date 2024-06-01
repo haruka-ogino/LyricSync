ec7a8c9482372db945803a749a3f9939f5491110
Haruka Ogino <harukariq@gmail.com>
2024-06-01 21:16:14 +1200

    tidy up

--- client/test/Nav.test.tsx
+++ client/test/Nav.test.tsx
@@ -1,8 +1,6 @@
 import { describe, it, expect } from 'vitest'
-import { render, screen } from '@testing-library/react'
+import { screen } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { RouterProvider, createMemoryRouter } from 'react-router-dom'
 import Nav from '../components/Nav.tsx'
 import { renderWithRouterAndQueryClient } from './setup.tsx'
 
