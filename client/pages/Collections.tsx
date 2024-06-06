6e131beac6a0756c3dab6a815350ffbcd70ead50
Haruka Ogino <harukariq@gmail.com>
2024-06-06 15:05:21 +1200

    update styling on collections display

--- client/pages/Collections.tsx
+++ client/pages/Collections.tsx
@@ -18,13 +18,17 @@ function Collections() {
         <p className="page-title">Collections</p>
         <ul>
           {data.map((collection, i) => (
-            <li key={i}>
+            <li key={i} className="collection-item">
               <Link to={`/collections/${collection.id}`}>
                 {collection.name}
               </Link>
+              <br />
               <Delete id={collection.id} />
             </li>
           ))}
+          <Link to="/collections/new-collection">
+            <li>+</li>
+          </Link>
         </ul>
       </>
     )
