bf1e36330643a79e3f8cf2f219a0919a10f84564
Moa Ha <moahaa03@gmail.com>
2024-06-13 15:17:35 +1200

    add user

--- server/db/user.ts
+++ server/db/user.ts
@@ -4,3 +4,11 @@ import { User } from '../../models/users'
 export async function getUsers() {
   return (await db('users')) as User[]
 }
+
+export async function getUserById(id: string) {
+  return await db('users').where({ id })
+}
+
+export async function addUser(data: User) {
+  await db('users').insert(data)
+}
--- server/routes/users.ts
+++ server/routes/users.ts
@@ -1,15 +1,32 @@
 import { Router } from 'express'
 import * as db from '../db/user'
-import checkJwt from '../auth0'
+import { JwtRequest } from '../auth0'
 const router = Router()
 
-router.get('/', checkJwt, async (req, res) => {
+router.get('/', async (req, res) => {
   try {
     const users = await db.getUsers()
     res.json(users)
   } catch (error) {
     console.log(error)
     res.status(500).json({ message: 'Something went wrong' })
+  }
+})
+
+router.post('/', async (req: JwtRequest, res) => {
+  const { id } = req.body
+  const { auth } = req
+  const existingUser = db.getUserById(id)
+  if (!existingUser) {
+    try {
+      await db.addUser(auth?.user.sub)
+      res.sendStatus(201).send('Added user')
+    } catch (error) {
+      console.log(error)
+      console.log(auth?.user.sub)
+
+      res.status(500).json({ message: 'Something went wrong adding user' })
+    }
   }
 })
 
