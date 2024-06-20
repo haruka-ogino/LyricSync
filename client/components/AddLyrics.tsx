b41103c8e8ae55068a0eea251f5945c245d0bdba
Moa Ha <moahaa03@gmail.com>
2024-06-20 17:44:48 +1200

    textarea form

--- client/components/AddLyrics.tsx
+++ client/components/AddLyrics.tsx
@@ -1,5 +1,35 @@
+import { useAddLyrics } from '../hooks/useSongs'
+
 function AddLyrics() {
-  return <>Add Lyrics`</>
+  const mutation = useAddLyrics()
+
+  function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault
+    console.log('add lyrics')
+  }
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="org-lyrics">Original Lyrics: </label>
+        <br />
+        <textarea
+          name="org-lyrics"
+          id="org-lyrics"
+          rows={10}
+          placeholder="Add the lyrics in original language"
+        />
+        <br></br>
+        <label htmlFor="trs-lyrics">Translated Lyrics: </label>
+        <br />
+        <textarea
+          name="trs-lyrics"
+          id="trs-lyrics"
+          rows={10}
+          placeholder="Add the lyrics in translated language"
+        />
+      </form>
+    </>
+  )
 }
 
 export default AddLyrics
