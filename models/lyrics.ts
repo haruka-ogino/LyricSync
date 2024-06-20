e520e5f62c9a81feab256ad25cb92855f8e9c405
Moa Ha <moahaa03@gmail.com>
2024-06-20 16:57:33 +1200

    make lyrics model

---
+++ models/lyrics.ts
@@ -0,0 +1,10 @@
+export interface Lyrics {
+  id: number
+  original_lang: number
+  trans_lang: number
+  original_lyric: string
+  trans_lyric: string
+  romanisation: boolean
+  romanised_text: string
+  song_id: number
+}
