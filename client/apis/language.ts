d59a3135048449bb142310753429739edae544a7
Moa Ha <moahaa03@gmail.com>
2024-07-12 15:31:23 +1200

    language api

---
+++ client/apis/language.ts
@@ -0,0 +1,19 @@
+import request from 'superagent'
+import { Language } from '../../models/languages'
+
+const rootUrl = '/api/v1/language'
+
+export async function getLanguage(): Promise<Language> {
+  try {
+    const res = await request.get(rootUrl)
+
+    if (!res.ok) {
+      throw new Error(`Failed to fetch Language (${res.status}): ${res.error}`)
+    }
+
+    return res.body as Language
+  } catch (error) {
+    console.error('Error fetching Language.')
+    throw new Error('Failed to fetch Language. Please try again.')
+  }
+}
