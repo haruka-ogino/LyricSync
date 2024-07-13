050e1150cf5a9a7f8c80003c0bcdb72e5801dd60
Moa Ha <moahaa03@gmail.com>
2024-07-13 21:38:29 +1200

    correct the function name

--- client/hooks/useLanguage.ts
+++ client/hooks/useLanguage.ts
@@ -1,9 +1,9 @@
 import { useQuery } from '@tanstack/react-query'
-import { getLanguage } from '../apis/language'
+import { getLanguages } from '../apis/language'
 
 export function useLanguage() {
   return useQuery({
     queryKey: ['language'],
-    queryFn: getLanguage,
+    queryFn: getLanguages,
   })
 }
