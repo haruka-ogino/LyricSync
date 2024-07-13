25ca56869c1839396e037fbd77781b95cacf1509
Moa Ha <moahaa03@gmail.com>
2024-07-13 22:08:22 +1200

    return array to map

--- client/apis/language.ts
+++ client/apis/language.ts
@@ -3,15 +3,14 @@ import { Language } from '../../models/languages'
 
 const rootUrl = '/api/v1/languages'
 
-export async function getLanguages(): Promise<Language> {
+export async function getLanguages(): Promise<Language[]> {
   try {
     const res = await request.get(rootUrl)
 
     if (!res.ok) {
       throw new Error(`Failed to fetch Language (${res.status}): ${res.error}`)
     }
-
-    return res.body as Language
+    return res.body
   } catch (error) {
     console.error('Error fetching Language.')
     throw new Error('Failed to fetch Language. Please try again.')
